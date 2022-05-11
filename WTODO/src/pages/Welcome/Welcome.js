2eaf1518e87c92412f85d31f056b7a518064991e
Muhammed Mertoğlu <av.muhammedmertoglu@gmail.com>
2022-05-11 18:24:06 +0300

    email sign page added

--- WTODO/src/pages/Welcome/Welcome.js
+++ WTODO/src/pages/Welcome/Welcome.js
@@ -1,19 +1,36 @@
-import React,{useEffect} from 'react'
-import {View,Text, Button,Image,Animated,Dimensions} from 'react-native';
+import React, { useEffect } from 'react'
+import { View, Text, Button, Image, Animated, Dimensions, TouchableOpacity } from 'react-native';
 import CheckLottie from '../../components/checkLottie/CheckLottie';
 import styles from './Welcome.style'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import * as BootSplash from 'react-native-bootsplash'
-
+import EmailSign from '../emailsign/EmailSign';
+import { GoogleSignin } from '@react-native-google-signin/google-signin'
+import auth from '@react-native-firebase/auth'
 
 const bootSplashLogo = require('../../../assets/images/launch_screen.jpg');
- 
-const Welcome = ({navigation}) => {
-    const [bootSplashIsVisible, setBootSplashIsVisible] = React.useState(true);
-  const [bootSplashLogoIsLoaded, setBootSplashLogoIsLoaded] =
-    React.useState(false);
+
+const Welcome = ({ navigation }) => {
+  const [bootSplashIsVisible, setBootSplashIsVisible] = React.useState(true);
+  const [bootSplashLogoIsLoaded, setBootSplashLogoIsLoaded] = React.useState(false);
+
   const opacity = React.useRef(new Animated.Value(1));
   const translateY = React.useRef(new Animated.Value(0));
+
+  const goEmailSign = () => {
+    navigation.navigate('EmailSignPage')
+  }
+
+  async function onGoogleButtonPress() {
+    try {
+      const { idToken } = await GoogleSignin.signIn();
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+      await auth().signInWithCredential(googleCredential);
+      navigation.navigate('HomePage')
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   const init = async () => {
     try {
@@ -44,11 +61,16 @@ const Welcome = ({navigation}) => {
   };
 
   React.useEffect(() => {
+    GoogleSignin.configure({
+      webClientId: '463873424393-tajuj60tq988kdfs6d9d2cmsqps0vr17.apps.googleusercontent.com',
+    });
     bootSplashLogoIsLoaded && init();
   }, [bootSplashLogoIsLoaded]);
-    return(
-            <View style={styles.container} >
-      {bootSplashIsVisible==true ? (
+
+
+  return (
+    <View style={styles.container} >
+      {bootSplashIsVisible == true ? (
         <Animated.View
           style={[
             styles.bootsplash,
@@ -66,34 +88,36 @@ const Welcome = ({navigation}) => {
             ]}
           />
         </Animated.View>
-      ):
-      <>
-      <View style={styles.lottie_view} >
-      <CheckLottie/>
-      </View>
-      <Text style={styles.header_text} >WTodo</Text> 
-      <Text style={styles.description_text} >Know What To Do</Text>
-      <View style={styles.privacy_container} >
-          <Text style={styles.andtext} >Sign In WeTodo</Text>
-          <View style={styles.privacy_container2} >
+      ) :
+        <>
+          <View style={styles.lottie_view} >
+            <CheckLottie />
+          </View>
+          <Text style={styles.header_text} >WTodo</Text>
+          <Text style={styles.description_text} >Know What To Do</Text>
+          <View style={styles.privacy_container} >
+            <Text style={styles.andtext} >Sign In WeTodo</Text>
+            <View style={styles.privacy_container2} >
               <Text style={styles.privacy_text} >Terms of use </Text>
               <Text style={styles.andtext} >and </Text>
               <Text style={styles.privacy_text} >Privacy Policy </Text>
-          </View>
-      </View>
-      <Text style={styles.withthistext} >Sign in with this</Text>
-      <View style={styles.lineview} ></View>
-      <View style={styles.auth_container}>
-      <View style={styles.google_container} >
-          <Image source={require('../../../assets/images/google.png')} style={styles.google_image} />
-      </View>
-      <Text>or</Text>
-      <View style={styles.email_container} >
-          <Icon name='email' size={36} color='black' />
-      </View>
-      </View></>}
             </View>
-        
-    )
+          </View>
+          <Text style={styles.withthistext} >Sign in with this</Text>
+          <View style={styles.lineview} ></View>
+          <View style={styles.auth_container}>
+            <TouchableOpacity
+              onPress={onGoogleButtonPress}
+              style={styles.google_container} >
+              <Image source={require('../../../assets/images/google.png')} style={styles.google_image} />
+            </TouchableOpacity>
+            <Text>or</Text>
+            <TouchableOpacity onPress={goEmailSign} style={styles.email_container} >
+              <Icon name='email' size={36} color='black' />
+            </TouchableOpacity>
+          </View></>}
+    </View>
+
+  )
 }
 export default Welcome
